a9d2f2e703cad0499a8f8d97d72d7f971c442ba2
Patrick <patrick.r.meadows@gmail.com>
2017-07-20 20:10:20 -0500

    Base project works.

--- src/main/java/org/jk/moneysaver/controller/HomeController.java
+++ src/main/java/org/jk/moneysaver/controller/HomeController.java
@@ -1,13 +1,21 @@
 package org.jk.moneysaver.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
